f1a8502567edaccd1e9096158227a89e1a4402ef
Mostafa <tnt.empire00@gmail.com>
2021-03-08 15:39:22 -0500

    Update arduinoCode.ino

--- arduinoCode.ino
+++ arduinoCode.ino
@@ -1,6 +1,11 @@
 #include <Servo.h>
 #include <LiquidCrystal.h>
 
+
+int bLeft = A0;
+int bRight = A5;
+
+//Ultrasonic sensor
 int echoPin = 6;
 int trigPin = 7;
 
@@ -9,6 +14,8 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 Servo motorX, motorY;
 
+int green = 3;
+int yellow = 9;
 
 void setup() {
   Serial.begin(9600);
@@ -18,13 +25,18 @@ void setup() {
   lcd.setCursor(0, 1);
   lcd.print("Angle: ");
 
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT); 
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
 
   motorX.attach(2);
   motorX.write(0);
 
   Serial.println("Startup");
+
+  pinMode(green, INPUT);
+  pinMode(yellow, INPUT);
+
+  motorY.attach(13);
 
 }
 
@@ -37,14 +49,31 @@ String inString;
 
 bool simple, vertical, complete;
 
+bool greenIn = false, yellowIn = false;
+
+bool pastGreen = false, pastYellow = false;
+
+int vertDelay = 100;
+int startTime, currTime;
+
 void loop() {
+  greenIn = digitalRead(green);
+  yellowIn = digitalRead(yellow);
+
+  if (greenIn == true && simple == false) {
+    simple = true;
+  }
+  if (yellowIn == true && vertical == false) {
+    vertical = true;
+  }
+
   if (Serial.available() > 0) {
     inString = Serial.readString();
   }
 
   if (inString == "Simple Sweep\n") {
     simple = true;
-  }else if (inString == "testing connection\n") {
+  } else if (inString == "testing connection\n") {
     Serial.println("Recieved\n");
   }
   if (simple == true) {
@@ -59,6 +88,22 @@ void loop() {
       dist = String(int(distance()));
       angle = String(x);
 
+      if (dist.toInt() < 100) {
+        if (angle.toInt() < 75) {
+          analogWrite(bRight, 200);
+          analogWrite(bLeft, LOW);
+        } else if (angle.toInt() > 115) {
+          analogWrite(bLeft, 200);
+          analogWrite(bRight, LOW);
+        } else {
+          analogWrite(bRight, 200);
+          analogWrite(bLeft, 200);
+        }
+      } else {
+        analogWrite(bRight, LOW);
+        analogWrite(bLeft, LOW);
+      }
+
       lcd.clear();
 
       lcd.setCursor(0, 0);
@@ -67,15 +112,41 @@ void loop() {
       lcd.setCursor(0, 1);
       lcd.print("Angle: " + angle);
 
-      Serial.println("D" + dist + "A" + angle+"\n");
+      Serial.println("D" + dist + "A" + angle + "\n");
       delay(40);
     }
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("N/A");
+    lcd.print("Sweep Done");
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Distance: ");
+    lcd.setCursor(0, 1);
+    lcd.print("Angle: ");
 
   }
+  analogWrite(bRight, LOW);
+  analogWrite(bLeft, LOW);
+
+  if (vertical == true) {
+    motorY.write(95);
+    startTime = millis();
+    while (currTime < vertDelay) {
+      lcd.clear();
+
+      lcd.setCursor(0, 0);
+      lcd.print("Distance: " + dist);
+      Serial.println("D" + dist + "A" + angle + "\n");
+      currTime = millis - startTime;
+
+    }
+    motorY.write(90);
+  }
+
 }
+
+
 
 double distance() {
   digitalWrite(trigPin, LOW);
